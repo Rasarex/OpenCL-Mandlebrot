a7602729050b2642d1d6861342f979d47aacdff6
grajcar-neosmart <130540362+grajcar-neosmart@users.noreply.github.com>
2025-02-28 14:43:30 +0100

    Add HSL support Add smothing to colors

--- kernel.cl
+++ kernel.cl
@@ -5,40 +5,81 @@ struct Dims {
   float ymax;
   int width;
   int height;
+  float zoom;
 };
-__kernel void mandlebrot(__global uchar *RGB,__global struct Dims *dims)
-{ 
+void HSLToRGB(int h, float s, float l, __global uchar *RGB, int id);
+float HueToRGB(float v1, float v2, float vH);
+__kernel void mandlebrot(__global uchar *RGB, __global struct Dims *dims) {
   int id = get_global_id(0);
   int w = dims->width;
   int h = dims->height;
   float fid = (float)id;
-  int w2 = w*w;
-  float col = (float)(id %w);
-  int sum = 0;
-  int sub_sample_size  = 3;
-  for(int s = 0; s < sub_sample_size * sub_sample_size; s += 1){
-    float sx = (float)(s % sub_sample_size) - 0.5;
-    float sy = (float)(s / sub_sample_size) - 0.5;
-    float p_x = (col +sx)/((float)h)  +  (dims->xmin);
-    float p_y = ((float)fid +sy) /((float)w2) +  (dims->ymin);
+  int w2 = w * w;
+  float col = (float)(id % w);
+  int max_iter = 1000;
+    float p_x = (col) / ((float)h) + (dims->xmin);
+    float p_y = ((float)fid) / ((float)w2) + (dims->ymin);
     float x = 0.0;
     float y = 0.0;
     float next_x = 0.0;
     float next_y = 0.0;
     int i = 0;
 
-    for(; i < 256; i += 1){
-      next_x = x*x - y*y + p_x;
-      next_y = x*y + x*y + p_y;
-      if(next_x*next_x + next_y*next_y > 4) break;
+    for (; i < max_iter; i += 1) {
+      next_x = x * x - y * y + p_x;
+      next_y = x * y + x * y + p_y;
+      if (next_x * next_x + next_y * next_y > 1<<10){
+        break;
+      }
       x = next_x;
       y = next_y;
     }
-    sum += i;
-  }
-  float val = (float)(sum/(sub_sample_size * sub_sample_size) & 0xff);
-  RGB[3*id] = sqrt(val/256)*256;
-  RGB[3*id + 1] = 0x00;//(char)((id >> 8) % 0xff);
-  RGB[3*id + 2] = 0x00;//(char)(id % 0xff);
+  float val = (float)(i % max_iter);
+    if (i < max_iter){
+      float log_zn = log(sqrt(x*x + y*y));
+      float nu =  log2(log_zn / log((float)max_iter));
+      val = i - nu;
+    }
+  // little endian // BGR
+  float val_hue = (float)(val / (float)max_iter);
+  float offset = 220.0f;
+  float hue = val_hue == 0.0f ? 0.0f :  fmod((pown(val_hue * (360 -offset ), 2)/sqrt(val_hue * (360 - offset)) + offset )   , 360.0f);
+  HSLToRGB((int)hue, 1.0f,  sqrt(val/(float)max_iter), RGB, id);
+}
+void HSLToRGB(int h, float s, float l, __global uchar *RGB, int id) {
+  unsigned char r = 0;
+  unsigned char g = 0;
+  unsigned char b = 0;
 
+  if (s == 0) {
+    RGB[3 * id + 2] = RGB[3 * id + 1] = RGB[3 * id] = (unsigned char)(l * 255);
+  } else {
+    float v1, v2;
+    float hue = (float)h / 360;
+
+    v2 = (l < 0.5) ? (l * (1 + s)) : ((l + s) - (l * s));
+    v1 = 2 * l - v2;
+
+    RGB[3 * id + 2] = (unsigned char)(255 * HueToRGB(v1, v2, hue + (1.0f / 3)));
+    RGB[3 * id + 1] = (unsigned char)(255 * HueToRGB(v1, v2, hue));
+    RGB[3 * id] = (unsigned char)(255 * HueToRGB(v1, v2, hue - (1.0f / 3)));
+  }
+}
+float HueToRGB(float v1, float v2, float vH) {
+  if (vH < 0)
+    vH += 1;
+
+  if (vH > 1)
+    vH -= 1;
+
+  if ((6 * vH) < 1)
+    return (v1 + (v2 - v1) * 6 * vH);
+
+  if ((2 * vH) < 1)
+    return v2;
+
+  if ((3 * vH) < 2)
+    return (v1 + (v2 - v1) * ((2.0f / 3) - vH) * 6);
+
+  return v1;
 }
--- main.c
+++ main.c
@@ -1,27 +1,52 @@
 // C standard includes
+#include <math.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
 
 // OpenCL includes
+#define CL_USE_DEPRECATED_OPENCL_1_2_APIS
 #include <CL/cl.h>
 
-uint8_t getByte(uint32_t i, uint32_t off){
-  return (uint8_t)((i >> off*8) & 0xff);
+#ifdef __unix__
+#include <assert.h>
+#include <errno.h>
+
+  typedef int errno_t;
+  errno_t fopen_s(FILE **f, const char *name, const char *mode) {
+    errno_t ret = 0;
+    assert(f);
+    *f = fopen(name, mode);
+    /* Can't be sure about 1-to-1 mapping of errno and MS' errno_t */
+    if (!*f)
+      ret = errno;
+    return ret;
+  }
+#endif
+
+uint8_t getByte(uint32_t i, uint32_t off) {
+  return (uint8_t)((i >> off * 8) & 0xff);
 }
-uint8_t getByte16(uint16_t i, uint32_t off){
-  return (uint8_t)((i >> off*8) & 0xff);
+uint8_t getByte16(uint16_t i, uint32_t off) {
+  return (uint8_t)((i >> off * 8) & 0xff);
 }
 #define MAX_SOURCE_SIZE (0x1000000)
+#define errorf(format, err, ...)                                               \
+  do {                                                                         \
+    if (err) {                                                                 \
+      fprintf(stderr, format, err, __VA_ARGS__);                               \
+    }                                                                          \
+  } while (0)
 
-uint32_t BMP_HEADER(char* buff,uint32_t size,uint16_t height, uint16_t width){
+uint32_t BMP_HEADER(char *buff, uint32_t size, uint16_t height,
+                    uint16_t width) {
 
-  buff[0x00] ='B';
-  buff[0x01] ='M';
-  buff[0x02] = getByte(size,0);
-  buff[0x03] = getByte(size,1);
-  buff[0x04] = getByte(size,2);
-  buff[0x05] = getByte(size,3);
+  buff[0x00] = 'B';
+  buff[0x01] = 'M';
+  buff[0x02] = getByte(size, 0);
+  buff[0x03] = getByte(size, 1);
+  buff[0x04] = getByte(size, 2);
+  buff[0x05] = getByte(size, 3);
 
   buff[0x0A] = 0x20;
   buff[0x0B] = 0;
@@ -33,10 +58,10 @@ uint32_t BMP_HEADER(char* buff,uint32_t size,uint16_t height, uint16_t width){
   buff[0x10] = 0;
   buff[0x11] = 0;
 
-  buff[0x12] = getByte16(width,0);
-  buff[0x13] = getByte16(width,1);
-  buff[0x14] = getByte16(height,0);
-  buff[0x15] = getByte16(height,1);
+  buff[0x12] = getByte16(width, 0);
+  buff[0x13] = getByte16(width, 1);
+  buff[0x14] = getByte16(height, 0);
+  buff[0x15] = getByte16(height, 1);
 
   buff[0x16] = 1;
   buff[0x17] = 0;
@@ -51,169 +76,154 @@ struct Dims {
   float ymax;
   int width;
   int height;
+  float zoom;
 };
-int main()
-{
-    cl_platform_id platform_id = NULL;
-    cl_device_id device_id = NULL;
-    cl_context context = NULL;
-    cl_command_queue command_queue = NULL;
-    cl_mem Cmobj = NULL;
-    cl_mem Args = NULL;
-    cl_program program = NULL;
-    cl_kernel kernel = NULL;
-    cl_uint ret_num_devices;
-    cl_uint ret_num_platforms;
-    cl_int ret;
+int main() {
+  cl_platform_id platform_id = NULL;
+  cl_device_id device_id = NULL;
+  cl_device_id *devices_id = NULL;
+  cl_context context = NULL;
+  cl_command_queue command_queue = NULL;
+  cl_mem Cmobj = NULL;
+  cl_mem Args = NULL;
+  cl_program program = NULL;
+  cl_kernel kernel = NULL;
+  cl_uint ret_num_devices;
+  cl_uint ret_num_platforms;
+  cl_int ret;
 
-    int width = 7680;
-    int height = 4320;
-    char *C;
-    int size = 3 * width * height;
-    C = (char*)calloc(size, sizeof(char));
+  int width = 7680;
+  int height = 4320;
+  char *C;
+  int size = 3 * width * height;
+  C = (char *)calloc(size, sizeof(char));
 
-    FILE *fp;
-    const char fileName[] = "./kernel.cl";
-    size_t source_size;
-    char *source_str;
+  FILE *fp;
+  const char fileName[] = "./kernel.cl";
+  size_t source_size;
+  char *source_str;
 
-    /* Load kernel source file */
-    int err = fopen_s(&fp,fileName, "r");
-    if (!fp)
-    {
-        fprintf(stderr, "Failed to load kernel.");
-        exit(-1);
-    }
-    source_str = (char *)malloc(MAX_SOURCE_SIZE);
-    source_size = fread(source_str, 1, MAX_SOURCE_SIZE, fp);
-    fclose(fp);
+  /* Load kernel source file */
+  int err = fopen_s(&fp, fileName, "r");
+  if (!fp) {
+    fprintf(stderr, "Failed to load kernel.");
+    exit(-1);
+  }
+  source_str = (char *)malloc(MAX_SOURCE_SIZE);
+  source_size = fread(source_str, 1, MAX_SOURCE_SIZE, fp);
+  fclose(fp);
 
-
-    /* Get Platform/Device Information */
-    ret = clGetPlatformIDs(1, &platform_id, &ret_num_platforms); 
-    fprintf(stderr, "%d %d \r\n",ret,__LINE__);
-    ret = clGetDeviceIDs(platform_id, CL_DEVICE_TYPE_GPU, 1, &device_id, &ret_num_devices); 
-    fprintf(stderr, "%d %d \r\n",ret,__LINE__);
+  /* Get Platform/Device Information */
+  ret = clGetPlatformIDs(1, &platform_id, &ret_num_platforms);
+  errorf("%d at line %d \r\n", ret, __LINE__);
+  ret = clGetDeviceIDs(platform_id, CL_DEVICE_TYPE_GPU, 1, &device_id,
+                       &ret_num_devices);
+  errorf("%d at line %d \r\n", ret, __LINE__);
 
   /* Create OpenCL Context */
-    context = clCreateContext(NULL, 1, &device_id, NULL, NULL, &ret);
-    fprintf(stderr, "%d %d \r\n",ret,__LINE__);
+  context = clCreateContext(NULL, 1, &device_id, NULL, NULL, &ret);
+  errorf("%d at line %d \r\n", ret, __LINE__);
 
-   /*Create command queue */
-        command_queue = clCreateCommandQueue(context, device_id, 0, &ret);
-    fprintf(stderr, "%d %d \r\n",ret,__LINE__);
+  /*Create command queue */
+  command_queue = clCreateCommandQueue(context, device_id, 0, &ret);
+  errorf("%d at line %d \r\n", ret, __LINE__);
 
-    /*Create Buffer Object */
-    float cordx = -0.16498674718560924;
-    float cordy = -1.0394110666891514;
-    float zoom = 1;
-    Cmobj = clCreateBuffer(context, CL_MEM_READ_WRITE, size, NULL, &ret);
-    fprintf(stderr, "%d %d \r\n",ret,__LINE__);
-    Args = clCreateBuffer(context, CL_MEM_READ_WRITE, sizeof(struct Dims),  NULL, &ret);
-    struct Dims args;
-    float fwidth = (float)width;
-    float fheight = (float)height;
-    float ratio = max(fwidth,fheight)/min(fwidth,fheight);
-    args.xmin = (-ratio )/zoom + cordx;
-    args.xmax = (ratio)/zoom + cordx ; //-0.80;
-    args.ymin = (-1.0)/zoom -cordy;// ( ((float)height)/zoom/2.0) + cordy;
-    args.ymax = (1.0)/zoom -cordy;//(((float)height)/zoom/2.0) - cordy;//-0.80;
-    args.width = width;
-    args.height = height;
-     
-    /* Copy input data to the memory buffer */
-    ret = clEnqueueWriteBuffer(command_queue, Args, CL_TRUE, 0, sizeof(struct Dims), &args, 0, NULL, NULL);
-    fprintf(stderr, "%d %d \r\n",ret,__LINE__);
+  /*Create Buffer Object */
+  float cordx = -0.16498674718560924;
+  float cordy = -1.0394110666891514;
+  float zoom =1   ;
+  Cmobj = clCreateBuffer(context, CL_MEM_READ_WRITE, size, NULL, &ret);
+  errorf("%d at line %d \r\n", ret, __LINE__);
+  Args = clCreateBuffer(context, CL_MEM_READ_WRITE, sizeof(struct Dims), NULL,
+                        &ret);
+  struct Dims args;
+  float fwidth = (float)width;
+  float fheight = (float)height;
+  float ratio = fmaxf(fwidth, fheight) / fminf(fwidth, fheight);
+  args.xmin = (-ratio) / zoom + cordx;
+  args.xmax = (ratio) / zoom + cordx; //-0.80;
+  args.ymin = (-1.0) / zoom - cordy;  // ( ((float)height)/zoom/2.0) + cordy;
+  args.ymax =
+      (1.0) / zoom - cordy; //(((float)height)/zoom/2.0) - cordy;//-0.80;
+  args.width = width;
+  args.height = height;
+  args.zoom = zoom;
 
-    /* Create kernel program from source file*/
-    program = clCreateProgramWithSource(context, 1, (const char **)&source_str, (const size_t *)&source_size, &ret);
-    fprintf(stderr, "%d %d \r\n",ret,__LINE__);
-    
-    ret = clBuildProgram(program, 1, &device_id, NULL, NULL, NULL);
-    if (ret != 0) {
-      // Determine the size of the log
-      size_t log_size;
-      clGetProgramBuildInfo(program, device_id, CL_PROGRAM_BUILD_LOG, 0, NULL, &log_size);
+  /* Copy input data to the memory buffer */
+  ret = clEnqueueWriteBuffer(command_queue, Args, CL_TRUE, 0,
+                             sizeof(struct Dims), &args, 0, NULL, NULL);
+  errorf("%d at line %d \r\n", ret, __LINE__);
+  /* Create kernel program from source file*/
+  program = clCreateProgramWithSource(context, 1, (const char **)&source_str,
+                                      (const size_t *)&source_size, &ret);
+  errorf("%d at line %d \r\n", ret, __LINE__);
 
-      // Allocate memory for the log
-      char *log = (char *) calloc(log_size,sizeof(char));
+  ret = clBuildProgram(program, 1, &device_id, NULL, NULL, NULL);
+  if (1 || ret != 0) {
+    size_t log_size;
+    clGetProgramBuildInfo(program, device_id, CL_PROGRAM_BUILD_LOG, 0, NULL,
+                          &log_size);
 
-      // Get the log
-      clGetProgramBuildInfo(program, device_id, CL_PROGRAM_BUILD_LOG, log_size, log, NULL);
+    char *log = (char *)calloc(log_size, sizeof(char));
 
-      // Print the log
-      printf("%s %s\n", log,log);
-    }
-    fprintf(stderr, "%d %d \r\n",ret,__LINE__);
+    clGetProgramBuildInfo(program, device_id, CL_PROGRAM_BUILD_LOG, log_size,
+                          log, NULL);
+    printf("%s\n", log);
+    free(log);
+  }
 
-    /* Create data parallel OpenCL kernel */
-    kernel = clCreateKernel(program, "mandlebrot", &ret);
-    fprintf(stderr, "%d %d \r\n",ret,__LINE__);
+  errorf("%d at line %d \r\n", ret, __LINE__);
+  /* Create data parallel OpenCL kernel */
+  kernel = clCreateKernel(program, "mandlebrot", &ret);
+  errorf("%d at line %d \r\n", ret, __LINE__);
 
-    /* Set OpenCL kernel arguments */
-    ret = clSetKernelArg(kernel, 0, sizeof(cl_mem), (void *)&Cmobj);
-    fprintf(stderr, "%d %d \r\n",ret,__LINE__);
-    ret = clSetKernelArg(kernel, 1, sizeof(cl_mem), (void*) &Args );
-    fprintf(stderr, "%d %d \r\n",ret,__LINE__);
-    // ret = clSetKernelArg(kernel, 2, sizeof(cl_mem), (void*) (&Args + sizeof(int)));
-    // ret = clSetKernelArg(kernel, 3, sizeof(cl_mem), (void*)(&Args + sizeof(int) * 2));
+  /* Set OpenCL kernel arguments */
+  ret = clSetKernelArg(kernel, 0, sizeof(cl_mem), (void *)&Cmobj);
+  errorf("%d at line %d \r\n", ret, __LINE__);
+  ret = clSetKernelArg(kernel, 1, sizeof(cl_mem), (void *)&Args);
+  errorf("%d at line %d \r\n", ret, __LINE__);
+  // ret = clSetKernelArg(kernel, 2, sizeof(cl_mem), (void*) (&Args +
+  // sizeof(int))); ret = clSetKernelArg(kernel, 3, sizeof(cl_mem),
+  // (void*)(&Args + sizeof(int) * 2));
 
-    size_t global_item_size = width*height;
-    size_t local_item_size = 1;
+  size_t global_item_size = width * height;
+  size_t local_item_size = 1;
 
-    /* Execute OpenCL kernel as data parallel */
-    ret = clEnqueueNDRangeKernel(command_queue, kernel, 1, NULL,
-                                 &global_item_size, &local_item_size, 0, NULL, NULL);
-    fprintf(stderr, "%d %d \r\n",ret,__LINE__);
+  /* Execute OpenCL kernel as data parallel */
+  ret =
+      clEnqueueNDRangeKernel(command_queue, kernel, 1, NULL, &global_item_size,
+                             &local_item_size, 0, NULL, NULL);
+  errorf("%d at line %d \r\n", ret, __LINE__);
 
-    /* Transfer result to host */
-    ret = clEnqueueReadBuffer(command_queue, Cmobj, CL_TRUE, 0, size * sizeof(char), C, 0, NULL, NULL);
-    fprintf(stderr, "%d %d \r\n",ret,__LINE__);
+  /* Transfer result to host */
+  ret = clEnqueueReadBuffer(command_queue, Cmobj, CL_TRUE, 0,
+                            size * sizeof(char), C, 0, NULL, NULL);
+  errorf("%d at line %d \r\n", ret, __LINE__);
 
+  char *header = (char *)calloc(0x20, sizeof(char));
 
-    char* header = (char*) calloc(0x20,sizeof(char));  
-    // for(int i =0; i< width; i += 1){
-    //     for( int j =0; j < height; j +=1){
-    //       int offset = (j*width + i) * 3;
-    //       // printf("%04x %04x |",(unsigned char)C[offset +1],(unsigned char)C[offset+2] );
-    //       int id = width*j + i;
-    //       int w = 400;
-    //       int h = 400;
-    //       float fid = (float)id;
-    //       int w2 = w*w;
-    //       float col = (float)(id %w);
-    //       float p_x = (col)/((float)h) * 2.0 - 1.0  ;
-    //       float p_y = ((float)fid) /((float)w2)*2.0   - 1.0 ;
-    //        printf("%04f %04f |",p_x,p_y);
-    //   }
-    //   printf("\r\n");
-    // }
+  /* Display Results */
+  FILE *f;
+  err = fopen_s(&f, "img.bmp", "wb");
+  BMP_HEADER(header, size, height, width);
+  fwrite(header, sizeof(char), 0x20, f);
+  fwrite(C, sizeof(char), size, f);
+  fclose(f);
 
-    /* Display Results */
-    FILE* f;
-    err = fopen_s(&f,"img.bmp","wb");
-    BMP_HEADER(header,size,height,width);
-    fwrite(header,sizeof(char),0x20,f);
-    fwrite(C,sizeof(char),size,f);
-    fclose(f);
+  /* Finalization */
+  ret = clFlush(command_queue);
+  ret = clFinish(command_queue);
+  ret = clReleaseKernel(kernel);
+  ret = clReleaseProgram(program);
+  ret = clReleaseMemObject(Cmobj);
+  ret = clReleaseMemObject(Args);
+  ret = clReleaseCommandQueue(command_queue);
+  ret = clReleaseContext(context);
 
-    /* Finalization */
-    ret = clFlush(command_queue);
-    ret = clFinish(command_queue);
-    ret = clReleaseKernel(kernel);
-    ret = clReleaseProgram(program);
-    ret = clReleaseMemObject(Cmobj);
-    ret = clReleaseMemObject(Args);
-    ret = clReleaseCommandQueue(command_queue);
-    ret = clReleaseContext(context);
+  free(source_str);
 
-    free(source_str);
+  free(header);
+  free(C);
 
-    free(header);
-    free(C);
-
-    
-    return ret;
+  return ret;
 }
-
-
